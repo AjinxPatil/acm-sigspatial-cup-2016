e0ed9387760a2b9b0454f7bf65889e86a8b3aed1
Ajinkya Patil <ajinx.patil@gmail.com>
2016-11-26 09:44:03 -0700

    Initial project structure

---
+++ geosparky-giscup/src/main/java/com/giscup2016/GoHotspotFinder.java
@@ -0,0 +1,13 @@
+package com.giscup2016;
+
+/**
+ * Hello world!
+ *
+ */
+public class GoHotspotFinder
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
