4640748134b28271de9706790bc77197bb0144de
ajinxpatil <ajinx.patil@gmail.com>
2016-12-03 15:19:55 -0700

    Unnecessary broadcast variables removed. Longitude/Latitude logic refactored.

--- geosparky-giscup/src/main/java/com/giscup2016/GeoHotspotConstants.java
+++ geosparky-giscup/src/main/java/com/giscup2016/GeoHotspotConstants.java
@@ -7,31 +7,21 @@ package com.giscup2016;
  * @since 1.0
  */
 public class GeoHotspotConstants {
-    public static final double LONGITUDE_MIN = -74.25;
-    public static final double LONGITUDE_MAX = -73.7;
-    public static final double LATITUDE_MIN = 40.5;
-    public static final double LATITUDE_MAX = 40.9;
+    public static final int LONGITUDE_MIN = -7425;
+    public static final int LONGITUDE_MAX = -7370;
+    public static final int LATITUDE_MIN = 4050;
+    public static final int LATITUDE_MAX = 4090;
     public static final int DAYS_MAX = 31;
-    public static final double CELL_X = 0.01;
-    public static final double CELL_Y = 0.01;
-    public static final double CELL_Z = 1;
 
     public static int gridRows() {
-    	return (int) ((LONGITUDE_MAX - LONGITUDE_MIN) / CELL_X);
-        
+        return LONGITUDE_MAX - LONGITUDE_MIN;
     }
 
     public static int gridColumns() {
-        return (int) ((LATITUDE_MAX - LATITUDE_MIN) / CELL_Y);
+        return LATITUDE_MAX - LATITUDE_MIN;
     }
 
     public static int gridCells() {
-        final int rows = (int) Math.round(100 * (LONGITUDE_MAX - LONGITUDE_MIN));
-        final int columns = (int) Math.round(100.0 * (LATITUDE_MAX - LATITUDE_MIN));
-        return rows * columns * DAYS_MAX;
-    }
-    
-    public static void main(String[] args) {
-    	System.out.println(gridRows() + ", " + gridColumns());
+        return gridRows() * gridColumns() * DAYS_MAX;
     }
 }
--- geosparky-giscup/src/main/java/com/giscup2016/GeoHotspotLocator.java
+++ geosparky-giscup/src/main/java/com/giscup2016/GeoHotspotLocator.java
@@ -1,17 +1,15 @@
 package com.giscup2016;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.broadcast.Broadcast;
-
 import scala.Tuple2;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * GeoHotspotLocator
@@ -20,32 +18,30 @@ import scala.Tuple2;
  * @since 1.0
  */
 public class GeoHotspotLocator {
-	
-	static class GetisOrdComparator implements Serializable, Comparator<Tuple2<Cell, Double>> {
 
-		@Override
-		public int compare(Tuple2<Cell, Double> o1, Tuple2<Cell, Double> o2) {
-			return o1._2.compareTo(o2._2);
-		}
-		
-	}
-	
+    private static class GetisOrdComparator implements Serializable, Comparator<Tuple2<Cell, Double>> {
+        @Override
+        public int compare(Tuple2<Cell, Double> o1, Tuple2<Cell, Double> o2) {
+            return o1._2.compareTo(o2._2);
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: Remove config
         final SparkConf conf = new SparkConf().setAppName("geosparky-giscup");
-                //.setMaster("local[1]").set("spark.driver.host", "127.0.0.1");
+        //.setMaster("local[1]").set("spark.driver.host", "127.0.0.1");
         final JavaSparkContext sc = new JavaSparkContext(conf);
-        final JavaPairRDD<Cell, Long> cellAttrs = sc.textFile(args[0]).filter(line -> isPointValid(line))
-                .mapToPair(line -> new Tuple2<>(createCell(line), 1L)).reduceByKey((x, y) -> x + y);
-        final Double s = calculateSValue(cellAttrs);
-        final Broadcast<Double> broadcastS = sc.broadcast(s);
+
+        final JavaPairRDD<Cell, Long> cellAttrs = sc.textFile(args[0]).mapToPair(line -> new Tuple2<>(createCell
+                (line), 1L)).filter(tuple -> isCellValid(tuple._1)).reduceByKey((x, y) -> x + y);
+
+        final double xBar = calculateXBar(cellAttrs);
+        final double s = calculateS(cellAttrs, xBar);
+
         final JavaPairRDD<Cell, Long> cellNetAttrValues = calculateCellNetAttrValue(cellAttrs);
-        final Double xBar = calculateXBar(cellAttrs);
-        final Broadcast<Double> broadcastXBar = sc.broadcast(xBar);
-        final Broadcast<Integer> broadcastN = sc.broadcast(GeoHotspotConstants.gridCells());
-        final JavaPairRDD<Cell, Double> getisOrd = cellNetAttrValues.mapToPair(a -> calculateGetisOrd(a, broadcastS,
-                broadcastXBar, broadcastN));
-        List<Tuple2<Cell, Double>> getisOrdTopFifty = getisOrd.top(50, new GetisOrdComparator());
+
+        final JavaPairRDD<Cell, Double> getisOrd = cellNetAttrValues.mapToPair(a -> calculateGetisOrd(a, s, xBar));
+        final List<Tuple2<Cell, Double>> getisOrdTopFifty = getisOrd.top(50, new GetisOrdComparator());
         final JavaRDD<Tuple2<Cell, Double>> getisOrd50 = sc.parallelize(getisOrdTopFifty);
         getisOrd50.saveAsTextFile(args[1]);
         sc.close();
@@ -76,43 +72,36 @@ public class GeoHotspotLocator {
         return neighborAttrValueRdd.reduceByKey((a, b) -> a + b);
     }
 
-    private static Double calculateSValue(final JavaPairRDD<Cell, Long> cellAttrValues) {
-        final JavaRDD<Long> attrValues = cellAttrValues.map(a -> a._2());
-        final Long netAttr = attrValues.reduce((a, b) -> a + b);
-        final Integer gridCellCount = GeoHotspotConstants.gridCells();
-        final Long netAttrSquared = attrValues.map(a -> a * a).reduce((a, b) -> a + b);
-        return Math.sqrt(netAttrSquared / (1.0 * gridCellCount) - Math.pow(netAttr / (1.0 * gridCellCount), 2));
-    }
-
     private static Double calculateXBar(final JavaPairRDD<Cell, Long> cellAttrValues) {
-        return (cellAttrValues.map(a -> a._2()).reduce((a, b) -> a + b)) / (1.0 * GeoHotspotConstants.gridCells());
+        return (cellAttrValues.map(a -> a._2).reduce((a, b) -> a + b)) / ((double) GeoHotspotConstants.gridCells());
     }
 
-    private static Tuple2<Cell, Double> calculateGetisOrd(final Tuple2<Cell, Long> getisOrdParameters,
-                                                          final Broadcast<Double> broadcastS, final
-                                                          Broadcast<Double> broadcastXBar, final Broadcast<Integer>
-                                                                  broadcastN) {
-        final Integer numberOfNeighbours = getisOrdParameters._1().getNn();
-        final Double getisOrd = (getisOrdParameters._2() - (broadcastXBar.value() * numberOfNeighbours)) /
-                (broadcastS.value() * Math.sqrt(((broadcastN.value() * numberOfNeighbours) - (numberOfNeighbours *
-                        numberOfNeighbours)) /
-                        (broadcastN.value() - 1)));
-        return new Tuple2<>(getisOrdParameters._1(), getisOrd);
+    private static Double calculateS(final JavaPairRDD<Cell, Long> cellAttrValues, final double xBar) {
+        final JavaRDD<Long> attrValues = cellAttrValues.map(a -> a._2());
+        final long netAttrSquared = attrValues.map(a -> a * a).reduce((a, b) -> a + b);
+        return Math.sqrt(netAttrSquared / ((double) GeoHotspotConstants.gridCells()) - Math.pow(xBar, 2));
     }
 
-    private static boolean isPointValid(final String line) {
-        final String[] fields = line.split(",");
-        final double lat = Double.parseDouble(fields[6]);
-        final double lon = Double.parseDouble(fields[5]);
-        return lat >= GeoHotspotConstants.LATITUDE_MIN && lat <= GeoHotspotConstants.LATITUDE_MAX
-                && lon >= GeoHotspotConstants.LONGITUDE_MIN && lon <= GeoHotspotConstants.LONGITUDE_MAX;
+    private static Tuple2<Cell, Double> calculateGetisOrd(final Tuple2<Cell, Long> cellNetAttrValues, final double
+            s, final double xBar) {
+        final int numberOfNeighbours = cellNetAttrValues._1().getNn();
+        final int N = GeoHotspotConstants.gridCells();
+        final Double getisOrd = (cellNetAttrValues._2() - xBar * numberOfNeighbours) /
+                (s * Math.sqrt((N * numberOfNeighbours - numberOfNeighbours * numberOfNeighbours) / (N - 1)));
+        return new Tuple2<>(cellNetAttrValues._1(), getisOrd);
+    }
+
+    private static boolean isCellValid(final Cell cell) {
+        return cell.getX() >= 0 && cell.getX() <= GeoHotspotConstants.gridColumns() &&
+                cell.getY() >= 0 && cell.getY() <= GeoHotspotConstants.gridRows();
     }
 
     private static Cell createCell(final String line) {
         final String[] fields = line.split(",");
-        final int x = (int) Math.floor(100.0 * (Double.parseDouble(fields[6]) - GeoHotspotConstants.LATITUDE_MIN));
-        final int y = (int) Math.floor(100.0 * Math.abs(Double.parseDouble(fields[5]) - GeoHotspotConstants
-                .LONGITUDE_MIN));
+        final int lat = (int) (100.0 * Double.parseDouble(fields[6]));
+        final int lon = (int) (100.0 * Double.parseDouble(fields[5]));
+        final int x = lat - GeoHotspotConstants.LATITUDE_MIN;
+        final int y = -1 * (lon - GeoHotspotConstants.LONGITUDE_MIN);
         final int z = Integer.parseInt(fields[1].split(" ")[0].split("-")[2]);
         return new Cell(x, y, z);
     }
